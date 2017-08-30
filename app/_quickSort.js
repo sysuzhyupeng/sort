d5f5161f6c6665deb33770783be33064565ee343
sysuzhyupeng <673341297@qq.com>
2017-08-30 15:00:05 +0800

    change quick sort

--- app/_quickSort.js
+++ app/_quickSort.js
@@ -4,23 +4,28 @@ function swap(arr, i, j){
 	arr[i] = arr[j];
 	arr[j] = temp;
 }
-function partition(arr, left, right){//分区操作，
-    var pivotValue = arr[right]//最右面设为标准
-    var storeIndex = left;
-    for(var i = left;i < right;i++){
+function partition(arr, left, right){
+	//原地分区, 将数组分为左小右大
+    var pivotValue = arr[right],
+    	//最右面设为标准
+    	storeIndex = left;
+    for(var i = left; i < right; i++){
+    	//将所有小于pivotValue放到前面
         if(arr[i] <= pivotValue){
             swap(arr, storeIndex, i);
             storeIndex++;
         }
     }
-    swap(arr,right,storeIndex);
-    return storeIndex//返回标杆元素的索引值
+    //最后将privot放到应有的位置上
+    swap(arr, right, storeIndex);
+    //返回标杆元素的索引值
+    return storeIndex;
 }
-function sort(arr,left,right){
+function sort(arr, left, right){
     if(left > right) return;
-    var storeIndex= partition(arr, left, right);
-    sort(arr, left,storeIndex-1);
-    sort(arr, storeIndex+1, right);
+    var storeIndex = partition(arr, left, right);
+    sort(arr, left, storeIndex - 1);
+    sort(arr, storeIndex + 1, right);
 }
 function quickSort(arr){
 	sort(arr, 0, arr.length - 1);
